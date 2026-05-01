ae636a2852e20e55b94413d2d4ce8b7346641d9b
okewunmi <git config --global user.email>
2026-05-01 01:35:41 +0100

    git commit -m "add node-appwrite dependency" git push

--- app/api/keepalive/route.js
+++ app/api/keepalive/route.js
@@ -37,7 +37,7 @@ const config = {
   projectId: "694ec3480029a8f2b0c0",
   DATABASE_ID: "694ee58c0003eac348b9",
   USERS_COLLECTION_ID: "userauth",
-APPWRITE_API_KEY: "standard_47ad55a574190be17f42e0d672f1f1ab070f52b6ce9e94ecdc02e974e88c9e38769b8cc74a7dc12f70e4df6154c64de119726dbcc381ac7eb4e2baf923c6506b7be505bea6285aecbe14e2f89ae264536a44c2197c1fb49eb650d1e86ddc09d09aef0fb9d1120f82a0a17c0edf01f7f979a6e7b3591e42548fac6bd01a7d358c",
+APPWRITE: "standard_47ad55a574190be17f42e0d672f1f1ab070f52b6ce9e94ecdc02e974e88c9e38769b8cc74a7dc12f70e4df6154c64de119726dbcc381ac7eb4e2baf923c6506b7be505bea6285aecbe14e2f89ae264536a44c2197c1fb49eb650d1e86ddc09d09aef0fb9d1120f82a0a17c0edf01f7f979a6e7b3591e42548fac6bd01a7d358c",
 };
 
 export async function GET() {
@@ -45,7 +45,7 @@ export async function GET() {
     const client = new Client()
       .setEndpoint(config.endpoint)
       .setProject(config.projectId)
-      .setKey(config.APPWRITE_API_KEY);
+      .setKey(config.APPWRITE);
 
     const databases = new Databases(client);
 
